30335e720c4e1b1bce64643ccab318cbf5cc58a5
smit <smithajgude2006@gmail.com>
2026-08-07 16:22:15 +0530

    fourth commit

---
+++ 4-containsduplicate.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+#include<vector>
+using namespace std;
+
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
