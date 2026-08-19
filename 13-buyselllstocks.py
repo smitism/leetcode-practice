babe7423caedd19cc3cd4bb8d9494878df2ac628
smit <smithajgude2006@gmail.com>
2026-08-19 17:20:16 +0530

    13th

---
+++ 13-buyselllstocks.py
@@ -0,0 +1,17 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r = 0,1
+        max_profit = 0
+        while r!=len(prices):
+            if prices[l]<prices[r]:
+                prof = prices[r]-prices[l]
+                max_profit = max(max_profit,prof)
+
+            else:
+                l=r
+            
+            r+=1
+        return max_profit
