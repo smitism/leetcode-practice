3915299cd000d16decbc64dbc0d01ec39baf8ba1
smit <smithajgude2006@gmail.com>
2026-08-22 18:22:51 +0530

    17th

---
+++ 17-minabsdiff.py
@@ -0,0 +1,20 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+
+
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        min_diff = float('inf')
+        for i in range(1,len(arr)):
+            min_diff = min(min_diff , abs(arr[i]-arr[i-1]))
+
+        result = []
+
+        for i in range(1,len(arr)):
+            if arr[i]-arr[i-1]==min_diff:
+                result.append([arr[i-1],arr[i]])
+        
+        return result
