3d3215fa62467dba18d37906fc45658ebbafd346
smit <smithajgude2006@gmail.com>
2026-08-24 15:24:22 +0530

    19th

---
+++ 19-singleno.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for i in nums:
+            
+            xor^=i
+        
+        return xor
