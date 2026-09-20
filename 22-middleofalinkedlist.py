ba18c61aeee452b2384f4375cfc257ca9129c8df
smit <smithajgude2006@gmail.com>
2026-09-20 15:12:25 +0530

    23st

---
+++ 22-middleofalinkedlist.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from pythonforleetcode import ListNode
+
+
+class Solution:
+    def middleNode(self, head: ListNode | None) -> ListNode | None:
+        i = j = head
+        while i and i.next:
+            j=j.next
+            i=i.next.next
+        return j
+        
